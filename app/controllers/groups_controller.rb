b4303eb6e371ec87fcad445ff887afe0911dc470
ryusuketake <tkd09043625429@icloud.com>
2019-11-01 21:41:28 +0900

    Create-Group-Edit-Function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,8 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
   
   def new
     @group = Group.new
@@ -13,16 +17,21 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+  
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
   end
 
-  def edit
+  def set_group
+    @group = Group.find(params[:id])
   end
-
-  def update
-  end
-
 end
